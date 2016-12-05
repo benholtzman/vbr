04effed358ed927d8ca7d354a8e9938a6246c4a0
benholtzman <benh@ldeo.columbia.edu>
2016-12-05 17:55:14 -0500

    adding function make_meltStep.m

---
+++ vbr/2_PLATES/4pt0_prem_forward/01_functions/make_meltStep.m
@@ -0,0 +1,28 @@
+function[step_vec] = make_step(N,N_z_steps,i_mid_step)
+% make a step
+% N = length of vector
+% N_z_steps = length of 
+
+if mod(N_z_steps,2)==1
+    N_z_steps = N_z_steps - 1;
+end
+
+x = linspace(-2,2,N_z_steps) ; 
+step = (0.5.*(erf(x)+1))  ;
+
+
+
+step_vec = (zeros(N,1)) ;   
+
+top_trans = i_mid_step - (N_z_steps/2) ;
+bot_trans = i_mid_step + (N_z_steps/2)-1  ;
+
+count = 1 ;
+for i = top_trans:(bot_trans-1)
+    step_vec(i) = step(count) ;
+    count = count+1 ;
+end
+
+for i = bot_trans:N
+    step_vec(i) = 1 ;
+end

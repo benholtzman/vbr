289eb879af0272f79718d224a20812c7f8eb0058
benholtzman <benh@ldeo.columbia.edu>
2018-07-11 15:25:06 -0400

    nice ! just got rid of some of the long comments.. will test some more...

--- vbr/4_VBR/VBR_v0p95/functions/Q_YT_maxwell.m
+++ vbr/4_VBR/VBR_v0p95/functions/Q_YT_maxwell.m
@@ -50,12 +50,12 @@ function[X_tau] = X_func(tau_norm_vec)
     beta2 = 1853.0 ;
     alpha2 = 0.5 ;
 
-    Beta  = beta1 .* ones(size(tau_norm_vec));    
-    Alpha = 0.39 - 0.28./(1+2.6*(tau_norm_vec.^0.1)); 
-    Beta(tau_norm_vec<1e-11)=beta2;     
-    Alpha(tau_norm_vec<1e-11)=alpha2;     
-    X_tau = Beta .* tau_norm_vec.^Alpha;    
-    
+    Beta  = beta1 .* ones(size(tau_norm_vec));
+    Alpha = 0.39 - 0.28./(1+2.6*(tau_norm_vec.^0.1));
+    Beta(tau_norm_vec<1e-11)=beta2;
+    Alpha(tau_norm_vec<1e-11)=alpha2;
+    X_tau = Beta .* tau_norm_vec.^Alpha;
+
 %    X_tau = zeros(1,length(tau_norm_vec)) ;
 %    for ff = 1:length(tau_norm_vec)
 %
@@ -69,8 +69,7 @@ function[X_tau] = X_func(tau_norm_vec)
 %      end
 %
 %    end
-        
-    
+
 end
 
 %% ===========================
@@ -84,9 +83,7 @@ end
 %  frequency dependent vars
    J1 = proc_add_freq_indeces(zeros(sz),n_freq);
    J2 = J1; Q = J1; Qinv = J1; M1 = J1; M2 = J1; M = J1; V = J1;
-   f_norm_glob=J1; tau_norm_glob=J1; 
-   %J1_gbs = J1; J2_gbs = J1; Q_gbs = J1; M_gbs = J1;
-   %J1_comp = J1; J2_comp = J1; Q_comp = J1; M_comp = J1; Va_comp = J1;
+   f_norm_glob=J1; tau_norm_glob=J1;
 
 %  vectorized rho and Vave
    rho_vec = reshape(rho_in,size(Mu_in(1:n_th)));
@@ -112,11 +109,11 @@ for x1 = 1:n_th  % loop using linear index!
   for i=1:n_freq
       i_glob = x1 + (i - 1) * n_th; % the linear index of the arrays with
                                    % a frequency index
-                                   
-      freq=f_vec(i); % current frequency 
-      f_norm=tau_mxw*freq; % normalized frequency           
-      max_tau_norm=1./(2*pi*f_norm); % maximum normalized tau 
-      
+
+      freq=f_vec(i); % current frequency
+      f_norm=tau_mxw*freq; % normalized frequency
+      max_tau_norm=1./(2*pi*f_norm); % maximum normalized tau
+
       tau_norm_f = max_tau_norm;%tau_norm(i) ;
       tau_norm_vec_local = linspace(0,tau_norm_f,100) ;
       %display(size(tau_norm_vec_local))
@@ -125,45 +122,22 @@ for x1 = 1:n_th  % loop using linear index!
 
       %FINT1 = trapz(X_tau) ;  %@(taup) (X_tau, taup
       %int1 = Tau_fac.*quad(FINT1, 0, tau_norm_i);
-      int1 = trapz(tau_norm_vec_local,X_tau./tau_norm_f) ; 
+      int1 = trapz(tau_norm_vec_local,X_tau./tau_norm_f) ;
 
       J1(i_glob) = Ju.*(1 + int1);
       %J2(i_glob) = Ju.*((pi/2)*X_tau(end) + 1/(2*pi*tau_mxw)); %this was wrong
       J2(i_glob) = Ju.*((pi/2)*X_tau(end) + tau_norm(i));
 
-      % if method==0
-      % %% MY METHOD--
-      %     D_vec = (alf.*Tau_X_vec.^(alf-1))./(Tau_H^alf - Tau_L^alf) ;
-      %
-      %     int_J1 = trapz(Tau_X_vec,(D_vec./(1+w^2.*Tau_X_vec.^2))) ;
-      %     J1(i_glob) = Ju.*(1+Delta.*int_J1) ;
-      %
-      %     int_J2 = trapz(Tau_X_vec,((Tau_X_vec.*D_vec)./(1+w^2.*Tau_X_vec.^2))) ;
-      %     J2(i_glob) = Ju.*(w*Delta*int_J2 + 1/(w*Tau_M)) ;
-
-      % elseif method==1
-      %% GEOFF's METHOD-- works !
-      % GA: try iterative quadrature
-      % (still seems to be a problem with high freq, w*tauH > 1 say)
-
-          %FINT1 = @(x) (x.^(alf-1))./(1+(w.*x).^2);
-          %int1 = Tau_fac.*quad(FINT1, 0, tau_norm_i);
-
-
-          %J1(i_glob) = Ju.*(1 + int1);
-          %J2(i_glob) = Ju.*(int2 + 1./(w.*Tau_M));
-      % end
-
-      J2_J1_frac=(1+sqrt(1+(J2(i_glob)./J1(i_glob)).^2))/2;      
-      Qinv(i_glob) = J2(i_glob)./J1(i_glob).*(J2_J1_frac.^-1);      
+      J2_J1_frac=(1+sqrt(1+(J2(i_glob)./J1(i_glob)).^2))/2;
+      Qinv(i_glob) = J2(i_glob)./J1(i_glob).*(J2_J1_frac.^-1);
       Q(i_glob) = 1./Qinv(i_glob);
       M1(i_glob) = 1./J1(i_glob) ;
       M2(i_glob) = 1./J2(i_glob) ;
-      M(i_glob) = 1./sqrt(J1(i_glob).^2+J2(i_glob).^2);%(M1(i_glob).^2 + M2(i_glob).^2).^(0.5) ;           
+      M(i_glob) = 1./sqrt(J1(i_glob).^2+J2(i_glob).^2);%(M1(i_glob).^2 + M2(i_glob).^2).^(0.5) ;
       V(i_glob) = sqrt(1./(J1(i_glob)*rho)).*(J2_J1_frac.^(-1/2));
 
       f_norm_glob(i_glob)=f_norm;
-      tau_norm_glob(i_glob)=tau_norm_f;  
+      tau_norm_glob(i_glob)=tau_norm_f;
       %Vave(x1) = Vave(x1) + V(i_glob); % add them all, divide by nfreq later
   % end loop over frequency
   end

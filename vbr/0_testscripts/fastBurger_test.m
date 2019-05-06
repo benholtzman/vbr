a446049dcd5efa3601c1fecd5f5d8049f3d2dfe1
chrishavlin <chris.havlin@gmail.com>
2019-05-06 15:16:17 -0500

    rn of 0_COOKBOOK to 0_testscripts

---
+++ vbr/0_testscripts/fastBurger_test.m
@@ -0,0 +1,127 @@
+%% ===================================================================== %%
+%%                     CB_003_JF10.m
+%% ===================================================================== %%
+%  Calls VBR after setting the elastic modulus to match the JF10
+%  modulus.
+%% ===================================================================== %%
+   clear
+
+%% ====================================================
+%% Load and set VBR parameters ========================
+%% ====================================================
+
+% put VBR in the path
+  path_to_top_level_vbr='../../';
+  addpath(path_to_top_level_vbr)
+  vbr_init
+
+%  write method list (these are the things to calculate)
+%  all methods will end up as output like:
+   VBR.in.elastic.methods_list={'anharmonic'};
+   VBR.in.anelastic.methods_list={'eBurgers'};
+   VBR.in.anelastic.eBurgers=Params_Anelastic('eBurgers');
+   VBR.in.anelastic.eBurgers.method='FastBurger';   
+
+%  frequencies to calculate at
+   VBR.in.SV.f = 1./logspace(-2,4,100);
+
+%% ====================================================
+%% Define the Thermodynamic State =====================
+%% ====================================================
+
+%  size of the state variable arrays. arrays can be any shape
+%  but all arays must be the same shape.
+   VBR.in.SV.T_K=700:50:1200;
+   VBR.in.SV.T_K=VBR.in.SV.T_K+273;
+   sz=size(VBR.in.SV.T_K); % temperature [K]
+
+%  intensive state variables (ISV)
+   VBR.in.SV.dg_um=3.1*ones(sz);
+   VBR.in.SV.P_GPa = 0.2 * ones(sz); % pressure [GPa]
+   VBR.in.SV.rho = 3300 * ones(sz); % density [kg m^-3]
+   VBR.in.SV.sig_MPa = 10 * ones(sz); % differential stress [MPa]
+   VBR.in.SV.chi=ones(sz); % composition fraction  1 = olivine, 0 = crust
+
+%  structural state variables (SSV)
+   VBR.in.SV.phi = 0.0 * ones(sz); % melt fraction
+
+%  compositional state variables (CSV)
+   VBR.in.SV.Ch2o = 0 * ones(sz) ; % water concentration
+
+%% ====================================================
+%% CALL THE VBR CALCULATOR ============================
+%% ====================================================
+
+   % run it initially (eBurgers uses high-temp background only by default)
+   [VBR_fastBurger] = VBR_spine(VBR) ;
+
+   % % adjust VBR input and get out eBurgers with background + peak
+   VBR.in.anelastic.eBurgers.method='PointWise';
+   [VBR_slowBurger] = VBR_spine(VBR) ;
+
+%% ====================================================
+%% Display some things ================================
+%% ====================================================
+
+  diff1=abs(VBR_fastBurger.out.anelastic.eBurgers.Qinv-VBR_slowBurger.out.anelastic.eBurgers.Qinv);
+  disp('max Qinv difference, abs(fast - slow):')
+  disp(max(diff1(1,:)))
+  disp('max Qinv % difference, 100* abs(fast - slow) / slow:')
+  smallQ=1e-4;
+  diff1=diff1(:)./(VBR_slowBurger.out.anelastic.eBurgers.Qinv+smallQ)*100;
+  disp(max(diff1(1,:)))
+
+  figure
+  subplot(1,2,1)
+  loglog(VBR.in.SV.f,squeeze(VBR_fastBurger.out.anelastic.eBurgers.Qinv(1,6,:)),'k','displayname','Fast')
+  hold all
+  loglog(VBR.in.SV.f,squeeze(VBR_slowBurger.out.anelastic.eBurgers.Qinv(1,6,:)),'--r','displayname','Slow')
+xlabel('f [Hz]'); ylabel('Qinv [GPa]')
+
+  subplot(1,2,2)
+  loglog(VBR.in.SV.f,squeeze(VBR_fastBurger.out.anelastic.eBurgers.M(1,6,:)),'k','displayname','Fast')
+  hold all
+  loglog(VBR.in.SV.f,squeeze(VBR_slowBurger.out.anelastic.eBurgers.M(1,6,:)),'--r','displayname','Slow')
+  legend('location','SouthEast')
+  xlabel('f [Hz]'); ylabel('M [GPa]')
+
+
+
+
+close all;
+figure;
+
+for iTemp = 1:numel(VBR.in.SV.T_K)
+
+  M_bg=squeeze(VBR.out.anelastic.eBurgers.M(1,iTemp,:)/1e9);
+  M_bg_peak=squeeze(VBR_with_peak.out.anelastic.eBurgers.M(1,iTemp,:)/1e9);
+  Q_bg=squeeze(VBR.out.anelastic.eBurgers.Qinv(1,iTemp,:));
+  Q_bg_peak=squeeze(VBR_with_peak.out.anelastic.eBurgers.Qinv(1,iTemp,:));
+  logper=log10(1./VBR.in.SV.f);
+  R=(iTemp-1) / (numel(VBR.in.SV.T_K)-1);
+  B=1 - (iTemp-1) / (numel(VBR.in.SV.T_K)-1);
+
+  subplot(2,2,1)
+  hold on
+  plot(logper,M_bg,'color',[R,0,B],'LineWidth',2);
+  ylabel('M [GPa] (background only) '); xlabel('period [s]')
+  ylim([20,80])
+
+  subplot(2,2,2)
+  hold on
+  plot(logper,log10(Q_bg),'color',[R,0,B],'LineWidth',2);
+  ylabel('Q^-1 (background only)'); xlabel('period [s]')
+  ylim([-2.5,0.5])
+
+  subplot(2,2,3)
+  hold on
+  plot(logper,M_bg_peak,'color',[R,0,B],'LineWidth',2);
+  ylabel('M [GPa] (background + peak) '); xlabel('period [s]')
+  ylim([20,80])
+
+  subplot(2,2,4)
+  hold on
+  plot(logper,log10(Q_bg_peak),'color',[R,0,B],'LineWidth',2);
+  ylabel('Q^-1 (background + peak)'); xlabel('period [s]')
+  ylim([-2.5,0.5])
+end

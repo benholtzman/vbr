77b2e1b27bc0c0178592797e3d6bec6889dbb28d
chrishavlin <chris.havlin@gmail.com>
2019-09-06 13:12:17 -0500

    cleaning the fudge

--- vbr/4_VBR/VBR_v0p95/params/Params_Anelastic.m
+++ vbr/4_VBR/VBR_v0p95/params/Params_Anelastic.m
@@ -87,11 +87,10 @@ function params = Params_Anelastic(method)
     params.Vstar = 10e-6 ; % m^3/mol (Activation Volume? or molar volume?)
     params.m = 1 ;
 
-    % the BUMP
+    % the peak
     params.Te = 0.1 ;
     params.Tgbs = 0.0833 ;% sec
     params.Delta = 0.3 ; % Relaxation strength
-    params.FUDGE = 1 ; %0.7 ;
   end
 
   if strcmp(method,'Andrade')

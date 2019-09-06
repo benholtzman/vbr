bcef71d45fd524afd0edd8182eb04c7b09b60ab4
chrishavlin <chris.havlin@gmail.com>
2019-09-06 11:49:41 -0500

    vectorized YT2016_solidus, now ~10x faster!

--- vbr/4_VBR/VBR_v0p95/functions/Q_YT2016_solidus.m
+++ vbr/4_VBR/VBR_v0p95/functions/Q_YT2016_solidus.m
@@ -55,49 +55,44 @@ function [VBR] = Q_YT2016_solidus(VBR)
     n_freq = numel(VBR.in.SV.f);
     sz = size(Gu_in);
     J1 = proc_add_freq_indeces(zeros(sz),n_freq);
-    J2 = J1; Q = J1; Qinv = J1; M1 = J1; M2 = J1; M = J1; V = J1;
-
-    % loop over points & frequencies
+    J2 = J1; V = J1;
     n_SVs=numel(Tn); % total elements in state variables
-    for i_SV=1:n_SVs % the state variable index
-      for i=1:n_freq % the frequency index
-        i_glob=i_SV + (i - 1) * n_SVs; % the global linear index in J1, J2, Q, etc.
 
-        p_p=period_vec(i)./(2*pi*tau_m(i_SV));
-        ABppa=A_B*(p_p^alpha_B);
-        lntaupp=log(tau_pp./p_p);
-        Ju0=Ju_in(i_SV);
-        J1_i=Ju0 .* (1 + ABppa/alpha_B+ ...
-             pifac*A_p(i_SV).*sig_p(i_SV).*(1-erf(lntaupp./(sqrt(2).*sig_p(i_SV)))));
-        J2_i=Ju0*pi/2.*(ABppa+A_p(i_SV).*(exp(-(lntaupp^2)./(2*sig_p(i_SV).^2)))) + ...
-             Ju0*p_p;
+    % loop over frequencies, calculate J1,J2,V 
+    for i = 1:n_freq
+      sv_i0=(i-1)*n_SVs + 1; % starting linear index of this freq
+      sv_i1=sv_i0+n_SVs-1; % ending linear index of this freq
 
-        % store in global
-        J1(i_glob)=J1_i;
-        J2(i_glob)=J2_i;
-        J2_J1_frac=(1+sqrt(1+(J2_i./J1_i).^2))/2;
-        Qinv(i_glob) = J2_i./J1_i.*(J2_J1_frac.^-1);
-        Q(i_glob) = 1./Qinv(i_glob);
-        M1(i_glob) = 1./J1_i ;
-        M2(i_glob) = 1./J2_i ;
-        M(i_glob) = 1./sqrt(J1_i.^2+J2_i.^2);%(M1(i_glob).^2 + M2(i_glob).^2).^(0.5) ;
-        V(i_glob) = sqrt(1./(J1_i*rho(i_SV))).*(J2_J1_frac.^(-1/2));
-      end
+      p_p=period_vec(i)./(2*pi*tau_m);
+      ABppa=A_B*(p_p.^alpha_B);
+      lntaupp=log(tau_pp./p_p);
+
+      J1(sv_i0:sv_i1)=Ju_in .* (1 + ABppa/alpha_B+ ...
+           pifac*A_p.*sig_p.*(1-erf(lntaupp./(sqrt(2).*sig_p))));
+      J2(sv_i0:sv_i1)=Ju_in*pi/2.*(ABppa+A_p.*(exp(-(lntaupp.^2)./(2*sig_p.^2)))) + ...
+           Ju_in.*p_p;
+
+      J2_J1_frac=(1+sqrt(1+(J2(sv_i0:sv_i1)./J1(sv_i0:sv_i1)).^2))/2;
+      V(sv_i0:sv_i1)=sqrt(1./(J1(sv_i0:sv_i1).*rho)).*(J2_J1_frac.^(-1/2));
     end
 
-    % WRITE VBR
-    VBR.out.anelastic.YT2016_solidus.J1 = J1;
-    VBR.out.anelastic.YT2016_solidus.J2 = J2;
-    VBR.out.anelastic.YT2016_solidus.M1 = M1;
-    VBR.out.anelastic.YT2016_solidus.M2 = M2;
-    VBR.out.anelastic.YT2016_solidus.Q = Q;
-    VBR.out.anelastic.YT2016_solidus.Qinv = Qinv;
-    VBR.out.anelastic.YT2016_solidus.M=M;
-    VBR.out.anelastic.YT2016_solidus.V=V;
+    % store and calculate other fields
+    VBRout.J1 = J1;
+    VBRout.J2 = J2;
+    VBRout.V=V;
+
+    J2_J1_frac=(1+sqrt(1+(J2./J1).^2))/2;
+    VBRout.M1 = 1./J1;
+    VBRout.M2 = 1./J2;
+    VBRout.Qinv = J2./J1.*(J2_J1_frac.^-1);
+    VBRout.Q = 1./VBRout.Qinv;
+    VBRout.M=1./sqrt(J1.^2+J2.^2);
 
     % calculate mean velocity along frequency dimension
-    VBR.out.anelastic.YT2016_solidus.Vave = Q_aveVoverf(V,VBR.in.SV.f);
+    VBRout.Vave = Q_aveVoverf(VBRout.V,VBR.in.SV.f);
 
+    % store the output structure
+    VBR.out.anelastic.YT2016_solidus=VBRout;
 
   end % end of has_solidus check
 end

9f4737ed9ad8960f2a69348c9ae9c3c2ce53893a
chrishavlin <chris.havlin@gmail.com>
2019-11-07 13:11:32 -0500

    mantle_extrap_FM: initial commit
    setting up directory structure

---
+++ Projects/mantle_extrap_FM/mantle_extrap.m
@@ -0,0 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% mantle_extrap.m
+%
+% - generates a range of mantle conditions from thermal modesl
+% - calculates mechnaical properties from the thermal models
+% - compares mechanical properties between methods for mantle conditions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
